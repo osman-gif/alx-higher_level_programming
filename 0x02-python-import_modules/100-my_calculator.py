f5e7583ab4b97f3ef5fb377de2812700447c8f27
osman-gif <osmanngong@gmail.com>
2023-11-02 23:38:45 +0200

    Modified the 100-calculator.py file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,35 +2,33 @@
 import sys
 import calculator_1 as cal
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-op = sys.argv[2]
-
-match op:
-    case '+':
-        summ = cal.add(a, b)
-        print("{} + {} = {}".format(a, b, summ))
-        exit(0)
-    case '-':
-        sub = cal.sub(a, b)
-        print("{} - {} = {}".format(a, b, sub))
-        exit(0)
-    case '*':
-        mul = cal.mul(a, b)
-        print("{} * {} = {}".format(a, b, mul))
-        exit(0)
-    case '/':
-        div = cal.div(a, b)
-        print("{} / {} = {}".format(a, b, int(div)))
-        exit(0)
-    case _:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
 if __name__ == "__main__":
-    import sys
-    sys.argv[0]
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = str(sys.argv[2])
+
+    match op:
+        case '+':
+            summ = cal.add(a, b)
+            print("{} + {} = {}".format(a, b, summ))
+            exit(0)
+        case '-':
+            sub = cal.sub(a, b)
+            print("{} - {} = {}".format(a, b, sub))
+            exit(0)
+        case '*':
+            mul = cal.mul(a, b)
+            print("{} * {} = {}".format(a, b, mul))
+            exit(0)
+        case '/':
+            div = cal.div(a, b)
+            print("{} / {} = {}".format(a, b, int(div)))
+            exit(0)
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
