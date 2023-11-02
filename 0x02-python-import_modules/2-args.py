de2a9639cc4560a9a8346812ba3b15467836c629
osman-gif <osmanngong@gmail.com>
2023-11-02 22:28:31 +0200

    Modified the 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-print("{} argument:".format(len(sys.argv) - 1))
+if len(sys.argv) != 2:
+    print("{} arguments:".format(len(sys.argv) - 1))
+else:
+    print("{} argument:".format(len(sys.argv) - 1))
 for i in range(1, len(sys.argv)):
     print("{}: {}".format(i, sys.argv[i]))
 
