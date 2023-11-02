bf750ba40ae9ff4709a4bb6b71e305a87b4101da
osman-gif <osmanngong@gmail.com>
2023-11-02 23:13:14 +0200

    Modified the 1-calculation.py file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,15 +5,13 @@ import calculate_1 as cal
 a = 10
 b = 5
 
-summ = cal.add(a, b)
-print("{} + {} = {}".format(a, b, summ))
-sub = cal.sub(a, b)
-print("{} - {} = {}".format(a, b, sub))
-mul = cal.mul(a, b)
-print("{} * {} = {}".format(a, b, mul))
-div = cal.div(a, b)
-print("{} / {} = {}".format(a, b, int(div)))
-
 if __name__ == "__main__":
-    import sys
-    sys.argv[0]
+
+    summ = cal.add(a, b)
+    print("{} + {} = {}".format(a, b, summ))
+    sub = cal.sub(a, b)
+    print("{} - {} = {}".format(a, b, sub))
+    mul = cal.mul(a, b)
+    print("{} * {} = {}".format(a, b, mul))
+    div = cal.div(a, b)
+    print("{} / {} = {}".format(a, b, int(div)))
