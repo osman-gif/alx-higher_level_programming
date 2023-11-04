84e5232068db846c4fc67b7e8207f9fdb7883224
osman-gif <osmanngong@gmail.com>
2023-11-04 16:43:02 +0200

    Added the 7-add_tuple.py file

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def set_tuple(tuple_c=()):
+    if len(tuple_c) == 0:
+        tuple_c = 0, 0
+    elif len(tuple_c) == 1:
+        tuple_c = tuple_c[0], 0
+    elif len(tuple_c) > 2:
+        tupe_a = tuple_c[0], tuple_c[1]
+    return tuple_c
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = set_tuple(tuple_a)
+    tuple_b = set_tuple(tuple_b)
+
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
