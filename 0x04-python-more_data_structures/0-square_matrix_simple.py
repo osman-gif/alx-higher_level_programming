a3924fc2683e68b73226671f5b55713baeabbf8e
osman-gif <osmanngong@gmail.com>
2023-11-08 16:09:04 +0200

    Modified the 0-square_matrix_simple.py file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 def square_matrix_simple(matrix=[]):
-
-        new = [[num**2 for num in matrix[idx]] for idx in range(len(matrix))]
-        return (new)
+    new = [[num**2 for num in matrix[idx]] for idx in range(len(matrix))]
+    return (new)
