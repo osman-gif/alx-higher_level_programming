8ca75b0d0e59594be6ff171e9d8a5f1ed9a59a1f
osman-gif <osmanngong@gmail.com>
2023-11-20 16:16:20 +0200

    Modified the 1-safe_print_integer.py file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     res = False
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
     except ValueError:
         res = False
     except TypeError:
