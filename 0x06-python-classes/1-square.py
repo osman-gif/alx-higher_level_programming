aac0c11d533a35914dfae3c8724ae73df1380462
osman-gif <osmanngong@gmail.com>
2023-11-21 23:04:19 +0200

    Added the 1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module creates a class square """
+
+
+class Square:
+
+    """ This class is to model a Square """
+
+    def __init__(self, __size):
+        self.__size = __size
