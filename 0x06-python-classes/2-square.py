0db3254876ef36424e72a89ed78f7747015a8708
osman-gif <osmanngong@gmail.com>
2023-11-22 00:13:37 +0200

    Modified the 2-square.py file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,11 +5,11 @@
 class Square:
     """ This class is to model a square """
 
-    def __init__(self, _size=0):
+    def __init__(self, __size=0):
 
         """ Initializes an instance of the class Square """
-        if type(_size) is not int:
+        if type(__size) is not int:
             raise TypeError("size must be an integer")
-        if _size < 0:
+        if __size < 0:
             raise ValueError("size must be >= 0")
-        self._size = _size
+        self.__size = __size
