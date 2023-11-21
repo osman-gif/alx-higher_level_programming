88d826bc0bf3958ee310bfcb5209999987865a6d
osman-gif <osmanngong@gmail.com>
2023-11-21 23:24:44 +0200

    Added the 5-sqaure.py file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" creates a class Square that defines a square """
+
+
+class Square:
+
+    """ This class is to model a square """
+    def area(self):
+
+        """ Returns the current squared area """
+        return self._size ** 2
+
+    def my_print(self):
+
+        """ prints in stdout the square with the character # """
+        if self._size == 0:
+            print("")
+
+        for i in range(0, self._size):
+            for j in range(0, self._size):
+                print("#", end="")
+            print("")
+
+    @property
+    def size(self):
+
+        """ gets the size of the current area """
+        return self._size
+
+    @size.setter
+    def size(self, _size):
+        """ sets the current size """
+
+        if type(_size) is not int:
+            raise TypeError("size must be an integer")
+        if _size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = _size
+
+    def __init__(self, _size=0):
+        """ Initilaizes an instance of square class """
+
+        self._size = _size
