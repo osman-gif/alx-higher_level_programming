38b85e219bee21836604917eeb91a6335b2cf85e
osman-gif <osmanngong@gmail.com>
2023-11-27 22:55:16 +0200

    Modified the 2-rectangle.py file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -41,9 +41,10 @@ class Rectangle:
             if value < 0:
                 raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
         return self.height * self.width
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
