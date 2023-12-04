68daab91aa8fd2dcb86b48d9767b11d73aa63f31
osman-gif <osmanngong@gmail.com>
2023-12-05 01:26:55 +0200

    Added  11-main.py 11-square.py 10-main.py 10-square.py files

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This module define a class that inherits form
+Rectangle class """
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """ This function initializes the Square class"""
+        self.__size = size
+
+        super().integer_validator("size", self.__size);
+
+    def area(self):
+        """ This function returns the area of the square """
+        return self.__size * self.__size
