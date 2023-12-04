37d30abba51e0c7238fd8ef0fdeb7ee2aa56f9cc
osman-gif <osmanngong@gmail.com>
2023-12-04 23:08:16 +0200

    Added the 4-inherits_from.py file

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+""" This module defines a function that recieves
+an object and a class, then check if  that object's type
+is of a class that inherits from the class (the second argument)
+"""
+
 
 def inherits_from(obj, a_class):
+    """ This function recieve an object and a class as
+    arguments, and returns true if the object's type
+    is of a class that inherits from the class (the second argument)
+    otherwise returns False """
     return issubclass(type(obj), a_class) and not (type(obj) is a_class)
