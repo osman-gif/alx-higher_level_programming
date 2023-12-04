c4f6893dec73b28b3b14538704568f4073dce73c
osman-gif <osmanngong@gmail.com>
2023-12-04 21:30:09 +0200

    Modified the 1-my_list.txt and 1-my_list.py files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-""" This module defines a class that returns a list """
+""" This module defines a class that inherts from list
+class and returns a list in ordered form. """
 
 
 class MyList(list):
-    """ This class inherits from list and returns the list ordered """
+    """ This class inherits from list and returns the list ordered form. """
 
     def print_sorted(self):
-        """ This function returns an ordered list """
+        """ This function returns an ordered list without affecting the
+        original list."""
         print((sorted(self)))
