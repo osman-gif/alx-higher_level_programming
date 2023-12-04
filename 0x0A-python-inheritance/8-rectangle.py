7c883f1536663633c1278fdbf3d1d2add4780ef5
osman-gif <osmanngong@gmail.com>
2023-12-05 00:49:45 +0200

    Added the 8-rectangle.py file

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module defines a class that inherit from antoher
+class called BaseGeometry"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """ This class inherits from the BaseGeomtry class
+    and validate the width and height attribues through
+    the inherited method validator"""
+    def __init__(self, width, height):
+        """ This function initilizes the Rectangle class
+        """
+
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
