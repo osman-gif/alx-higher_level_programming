451bf32c9aadf8f352367d4b1722ff15594c96b8
osman-gif <osmanngong@gmail.com>
2023-12-05 20:43:11 +0200

    Modified the 1-write_file.py and 0-read_file.py files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,7 @@ a file and return the content read """
 def read_file(filename=""):
     """ This function reads a file and return
     the content it read """
-    
+ 
     data = ""
     with open(filename, encoding="UTF8") as f:
         for line in f:
