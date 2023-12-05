2a426d13080632e1f0ae4928add5ed813bcb4e49
osman-gif <osmanngong@gmail.com>
2023-12-05 23:39:13 +0200

    Added 8-class_to_json.py file

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,12 +4,9 @@ the dictionary description with simple data structure
 for JSON serialization of an object """
 
 
-import json
-
-
 def class_to_json(obj):
     """ function that returns
     the dictionary description with simple data structure
     for JSON serialization of an object"""
 
-    return json.dumps(obj.__dict__)
+    return (obj.__dict__)
