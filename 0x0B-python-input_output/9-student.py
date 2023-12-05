73a77c355984cf2e875e4a4df15ee55c604e5781
osman-gif <osmanngong@gmail.com>
2023-12-05 23:50:11 +0200

    Added the 9-student.py file

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This module defines a class that models a student """
+""" This module defines a class that models a student
+object with one function"""
 
 
 class Student:
@@ -8,7 +9,8 @@ class Student:
     called to_json that returns json-like object """
 
     def __init__(self, first_name, last_name, age):
-        """ This function initializes the class insances"""
+        """ This function initializes all class instances
+        attributes """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
