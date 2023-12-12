4a52d3aaa11bef9e61cb83b003d04722ba37fae1
osman-gif <osmanngong@gmail.com>
2023-12-12 20:03:14 +0200

    Modified some files

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        self.r1 = Rectangle(2, 2, 2, 2)
+        self.kwargs = {"x": 10, "y": 20}
+
+    def test_init__(self):
+        with self.assertRaises(TypeError):
+            r2 = Rectangle()
+
+    def test_area(self):
+        self.assertEqual(self.r1.area(), 4)
+
+    def test_str(self):
+        self.assertEqual(
+                str(self.r1), '[Rectangle] (7) 2/2 -\n            2 2')
+
+    def test_update(self):
+        self.r1.update(**self.kwargs)
+        self.assertEqual(
+                str(self.r1), '[Rectangle] (9) 10/20 -\n            2 2')
+
+    def test_to_dictionary(self):
+        self.r1.to_dictionary()
+        self.assertEqual(
+                str(self.r1.to_dictionary()
+                    ), "{'id': 8, 'width': 2, 'height': 2, 'x': 2, 'y': 2}")
