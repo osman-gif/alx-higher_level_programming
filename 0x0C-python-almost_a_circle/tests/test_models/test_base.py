f9eb6dbf1b769852c1e580f080e56c8457c9b4e1
osman-gif <osmanngong@gmail.com>
2023-12-12 15:26:37 +0200

    Modified some files in tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,7 @@ from models.base import Base
 from models.square import Square
 from models.rectangle import Rectangle
 
+
 class TestBase(unittest.TestCase):
 
     def test_to_json_string(self):
@@ -13,7 +14,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(Base.to_json_string({}), "[]")
         self.assertEqual(Base.to_json_string(()), "[]")
         self.assertEqual(Base.to_json_string(""), "[]")
-        self.assertEqual(Base.to_json_string(Base.from_json_string("null")), "[]")
+        self.assertEqual(
+                Base.to_json_string(Base.from_json_string("null")), "[]")
         self.assertEqual(Base.to_json_string(Base.__name__), '"Base"')
 
         with self.assertRaises(TypeError):
@@ -21,13 +23,21 @@ class TestBase(unittest.TestCase):
         with self.assertRaises(TypeError):
             Base.to_json_string()
         with self.assertRaises(TypeError):
-            Base.to_json_string(3,3)
+            Base.to_json_string(3, 3)
 
     def test_from_json_string(self):
-        self.assertEqual(Base.from_json_string('{"key":"value"}'),{'key': 'value'})
-        self.assertEqual(Base.from_json_string('["key","value"]'),['key', 'value'])
-        self.assertEqual(Base.from_json_string(Base.to_json_string(None)), [])
-        self.assertEqual(Base.from_json_string('[{"key":"value"},{"key":"value"}]'), [{'key': 'value'}, {'key': 'value'}])
+        self.assertEqual(
+                Base.from_json_string(
+                    '{"key":"value"}'), {'key': 'value'})
+        self.assertEqual(
+                Base.from_json_string(
+                    '["key","value"]'), ['key', 'value'])
+        self.assertEqual(
+                Base.from_json_string(Base.to_json_string(None)), [])
+        self.assertEqual(
+                Base.from_json_string(
+                    '[{"key":"value"},{"key":"value"}]'),
+                [{'key': 'value'}, {'key': 'value'}])
         with self.assertRaises(TypeError):
             Base.from_json_string()
         with self.assertRaises(TypeError):
@@ -35,13 +45,13 @@ class TestBase(unittest.TestCase):
 
     def test_save_to_file(self):
 
-        self.assertFalse(Base.save_to_file([Rectangle(1,2), Rectangle(3,4)]))
-        self.assertFalse(Base.save_to_file([Square(1,2,3), Rectangle(2,3)]))
+        self.assertFalse(Base.save_to_file([Rectangle(1, 2), Rectangle(3, 4)]))
+        self.assertFalse(Base.save_to_file([Square(1, 2, 3), Rectangle(2, 3)]))
 
         with self.assertRaises(TypeError):
             Base.save_to_file()
         with self.assertRaises(AttributeError):
-            Base.save_to_file([3,4,4])
+            Base.save_to_file([3, 4, 4])
         with self.assertRaises(TypeError):
             Base.save_to_file(4)
         with self.assertRaises(NameError):
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -4,10 +4,11 @@ from models.base import Base
 from models.square import Square
 from models.rectangle import Rectangle
 
+
 class TestSquare(unittest.TestCase):
     def setUp(self):
         self.s1 = Square(2)
-        self.s2 = Square(2,2,2)
+        self.s2 = Square(2, 2, 2)
 
     def test_init__(self):
         with self.assertRaises(ValueError):
@@ -15,22 +16,26 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(TypeError):
             s3 = Square()
         with self.assertRaises(TypeError):
-            s3 = Square(1,1,1,1,1)
+            s3 = Square(1, 1, 1, 1, 1)
         with self.assertRaises(ValueError):
             s3 = Square(-2)
 
     def test_to_dictionary(self):
 
-        self.assertEqual(self.s1.to_dictionary(),{'id': 5, 'size': 2, 'x': 0, 'y': 0})
-        self.assertAlmostEqual(self.s2.to_dictionary(),{'id': 6, 'size': 2, 'x': 2, 'y': 2})
+        self.assertEqual(
+                self.s1.to_dictionary(),
+                {'id': 5, 'size': 2, 'x': 0, 'y': 0})
+        self.assertAlmostEqual(
+                self.s2.to_dictionary(),
+                {'id': 6, 'size': 2, 'x': 2, 'y': 2})
 
     def test_str__(self):
 
-        self.assertEqual(self.s2.__str__(),'[Square] (4) 2/2 - 2')
+        self.assertEqual(self.s2.__str__(), '[Square] (4) 2/2 - 2')
 
     def test_update(self):
-        kwargs = {"x":10, "y":20}
-        args = (1,1,1,1)
+        kwargs = {"x": 10, "y": 20}
+        args = (1, 1, 1, 1)
 
         self.s1.update(**kwargs)
         self.assertEqual(str(self.s1), '[Square] (7) 10/20 - 2')
@@ -38,6 +43,6 @@ class TestSquare(unittest.TestCase):
         self.s1.update(*args)
         self.assertEqual(str(self.s1), '[Square] (1) 1/1 - 1')
         self.assertEqual(str(self.s1), '[Square] (1) 1/1 - 1')
-        
+
         self.s1.update()
         self.assertEqual(str(self.s1), '[Square] (1) 1/1 - 1')
