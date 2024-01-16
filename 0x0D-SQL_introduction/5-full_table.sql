f3ea1ee5ddefce8efe0ff2179b33d86661997328
osman-gif <osmanngong@gmail.com>
2024-01-16 21:43:52 +0200

    Added 5-full_table.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints the full description of the table first_table from the database hbtn_0c_0
+USE hbtn_0c_0;
+-- full description of the table first_table from the database hbtn_0c_0
+SHOW CREATE TABLE first_table;
