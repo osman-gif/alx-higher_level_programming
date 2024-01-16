e404d835adccb11a72d99bb1141e19f9b26e8442
osman-gif <osmanngong@gmail.com>
2024-01-16 23:23:14 +0200

    Added the 11-best_score.sql file

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
-SELECT name, score FROM second_table WHERE score >= 10 GROUP BY name, score ORDER BY score;
+SELECT score, name FROM second_table WHERE score >= 10 GROUP BY score, name ORDER BY score DESC;
