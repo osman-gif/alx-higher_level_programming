9042f5cdd1030d29d3593cfc6585977600126776
osman-gif <osmanngong@gmail.com>
2024-01-16 21:24:06 +0200

    Modified 3-list_tables.sql 4-first_table.sql files

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,8 +1,2 @@
--- lists all the tables of a database
-
--- This command set the database passed to
--- the script as the current database
-SELECT DATABASE() AS current_database;
-
--- Lists the tables in the current database
+-- lists all the tables of a database in your MySQL server.
 SHOW TABLES;
