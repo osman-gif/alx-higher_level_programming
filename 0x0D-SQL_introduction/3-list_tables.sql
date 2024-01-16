358574db35acc7a7958655fe4d88378de38d4184
osman-gif <osmanngong@gmail.com>
2024-01-16 15:01:49 +0200

    Modified the 3-list_tables.sql file

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,8 @@
 -- lists all the tables of a database
 
--- Use the database passed '@arg1'
-USE @args1;
--- Lists the tables in the current table
+-- This command set the database passed to
+-- the script as the current database
+SELECT DATABASE() AS current_database;
+
+-- Lists the tables in the current database
 SHOW TABLES;
