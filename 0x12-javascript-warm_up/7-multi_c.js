3864d849728c59ceb7cf494c31c71de52ee3f459
osman-gif <osmanngong@gmail.com>
2024-02-13 00:10:15 +0200

    Modified the 7-multi_c.js file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,17 +4,15 @@
  */
 
 const argument = process.argv.slice(2);
+const num = argument[0];
 
-if (argument.length === 1) {
+if (argument.length === 1 && !isNaN(parseInt(num))) {
   const num = argument[0];
-
-  if (!isNaN(parseInt(num))) {
-    let count = 0;
-    while (count < num) {
-      console.log('C is fun');
-      count++;
-    }
-  } else { console.log('Missing number of occurrences'); }
-} else if (argument.length === 0) {
+  let count = 0;
+  while (count < num) {
+    console.log('C is fun');
+    count++;
+  }
+} else if (argument.length === 0 || isNaN(parseInt(num))) {
   console.log('Missing number of occurrences');
 }
