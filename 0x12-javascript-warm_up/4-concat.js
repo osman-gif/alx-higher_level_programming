8a80c4389055e86710a237dbd45a55c1f6dd2a3e
osman-gif <osmanngong@gmail.com>
2024-02-12 23:36:14 +0200

    Modified the 4-concat.js file

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -5,8 +5,6 @@
 
 const argument = process.argv.slice(2);
 
-if (argument.length === 2) {
-  const arg1 = argument[0];
-  const arg2 = argument[1];
-  console.log(arg1 + ' is ' + arg2);
-}
+const arg1 = argument[0];
+const arg2 = argument[1];
+console.log(arg1 + ' is ' + arg2);
