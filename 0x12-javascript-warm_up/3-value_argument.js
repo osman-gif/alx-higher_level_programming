6f5972ede6e6cdf99c32eacb78d3f7814a9d7e9e
osman-gif <osmanngong@gmail.com>
2024-02-12 23:30:34 +0200

    Initial commit

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * prints the first argument passed to it
+ */
+
+const argument = process.argv.slice(2);
+
+if (argument.length === 0) {
+  console.log('No argument');
+} else if (argument.length === 1) {
+  console.log(argument[0]);
+}
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * This script prints a message depending of the number of arguments passed
+ */
+
+const argument = process.argv.slice(2);
+
+if (argument.length === 2) {
+  const arg1 = argument[0];
+  const arg2 = argument[1];
+  console.log(arg1 + ' is ' + arg2);
+}
