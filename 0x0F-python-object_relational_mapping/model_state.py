4eab13fe4db850a143fd44d7e3360115528dd991
osman-gif <osmanngong@gmail.com>
2024-02-18 14:55:19 +0200

    Modified the model_state.py file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-
 """
  python file that contains the class definition of a State
  and an instance Base = declarative_base()
 """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+
 if __name__ == '__main__':
     # declare a mapping
     Base = declarative_base()
