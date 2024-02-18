0ad855199b5ff2fee801e89e1b90f5aa24b66eed
osman-gif <osmanngong@gmail.com>
2024-02-18 11:20:43 +0200

    Modified the 1-filter_states.py file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# script that lists all states with a name starting
-# with N (upper N) from the database hbtn_0e_0_usa
+
+""" script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
     import sys
