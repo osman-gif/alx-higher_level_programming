91ec0216a4956ff69c5eb62255b45af2250e994c
osman-gif <osmanngong@gmail.com>
2024-03-01 23:26:36 +0200

    Added the 6-post_email.py file

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ Python script that takes in a URL and an email address, sends
+ a POST request to the passed URL with the email as a parameter,
+ and finally displays the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    paylod = {'email': email}
+    response = requests.get(url, params=paylod)
+    content = response.content
+    print(content)
