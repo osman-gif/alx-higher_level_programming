107554971c0dd0d295e2c127f68ae9fbbfcead9b
osman-gif <osmanngong@gmail.com>
2024-03-02 10:29:04 +0200

    Modified the 10-my_github.py file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     url = 'https://api.github.com/user'
     my_auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     response = requests.get(url, auth=my_auth)
-    if response.status_code == 200:  
+    if response.status_code == 200:
         print(response.text.split(',')[1].split(':')[1])
     else:
         print(None)
