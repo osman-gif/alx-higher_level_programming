4d6e2c1931e7f4b19953bed2405124cc8eaf0258
osman-gif <77807296+osman-gif@users.noreply.github.com>
2024-05-06 20:57:09 +0200

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Bash script that takes in a URL, sends a GET request to the URL
-curl -s -L $1
--- 0x10-python-network_0/3-methods.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Bash script that takes in a URL and displays all HTTP methods
-curl -s -I $1 | grep Allow | cut -d":" -f2 | sed "s/ //"
