a82fbcba4f6042e468a414e41e1396ec292f8090
osman-gif <77807296+osman-gif@users.noreply.github.com>
2024-05-06 21:03:16 +0200

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
-
-request(url, function (error, response, body) {
-  !error ? console.log('code:', response.statusCode) : console.error(error);
-});
