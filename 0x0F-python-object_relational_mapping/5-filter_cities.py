dec4b7ef564ea760bf0fbcc8d5a4dcd2633cce27
osman-gif <osmanngong@gmail.com>
2024-06-25 09:53:22 +0200

    Modified the 5-filter_cities.py file

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""This script takes in the name of a state as an argument and lists
+ all cities of that state, using the database hbtn_0e_4_usa"""
 if __name__ == '__main__':
     import MySQLdb
     import sys
