dd396ff6c3c04204c44250678aa9d7c3e0d568e2
osman-gif <osmanngong@gmail.com>
2024-07-04 13:15:43 +0200

    Modified the 102-catch_me.sh file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script makes a request to 0.0.0.0:5000/catch_me, response = You got me!
-curl -s -X GET -d "You got me!" 0.0.0.0:5000/catch_me
+curl -s -X GET -d "You got me!" "0.0.0.0:5000/catch_me"
